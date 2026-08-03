cc4996e10ff0ec410ef4f5b54a9634506f5f32a5
Andrew <andrijshepit@gmail.com>
2026-08-03 21:51:38 +0300

    explanation of problems2

--- taxi/views.py
+++ taxi/views.py
@@ -16,13 +16,14 @@ def index(request):
     num_cars = Car.objects.count()
     num_manufacturers = Manufacturer.objects.count()
     num_visits = request.session.get("num_visits", 0)
+    #no it does not do this our counter starts from 1 in all times
     request.session["num_visits"] = num_visits + 1
 
     context = {
         "num_drivers": num_drivers,
         "num_cars": num_cars,
         "num_manufacturers": num_manufacturers,
-        "num_visits": num_visits + 1,
+        "num_visits": num_visits ,
 
     }
 
